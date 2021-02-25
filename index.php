9808a5c557c2fd8d73be489bb943b9edb705548b
Usernametaken420 <Nico_Gabin@yahoo.com>
2021-02-25 17:08:17 -0300

    Presentación arreglada
    Actualizada la presentación del sitio, ahora contando con una hoja de estilo y fondo.

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Sleepy's Page</title>
+    <link rel="stylesheet" href="style.css"/>
+  </head>
+
+  <body>
+    <!-- Barra de navegación -->
+    <?php
+      include "sidebar.inc";
+    ?>
+    <!-- Sección principal de la página -->
+    <div class="maintext">
+      <p>Just fuck my shit up, fam</p>
+    </div>
+  </body>
+</html>
